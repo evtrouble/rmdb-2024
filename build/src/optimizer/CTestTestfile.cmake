4c582ac0f786a423e47f31b1bbf8bb34a4e3028e
StarAstronaut2 <zhir.l1874@gmail.com>
2025-03-01 17:15:02 +0800

    客户端显示错误信息
    元数据管理与DDL语句
    
    DQL语句和DML语句
    
    连接查询未实现

--- build/src/optimizer/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nero/db2024/rmdb/src/optimizer
-# Build directory: /home/nero/db2024/rmdb/build/src/optimizer
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/src/storage/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nero/db2024/rmdb/src/storage
-# Build directory: /home/nero/db2024/rmdb/build/src/storage
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
