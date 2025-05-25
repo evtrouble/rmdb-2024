6d23fcb397e8e46b6d8b9df5d3651d41a6fca667
StarAstronaut2 <zhir.l1874@gmail.com>
2025-05-25 17:40:38 +0800

    replacer

---
+++ src/replacer/clock_replacer.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+#include <mutex>
+#include <atomic>
+#include "common/config.h"
+#include "replacer/replacer.h"
+
+class ClockReplacer : public Replacer
+{
+public:
+    explicit ClockReplacer(size_t num_pages);
+    ~ClockReplacer() = default;
+
+    bool victim(frame_id_t *frame_id) override;
+    void pin(frame_id_t frame_id) override;
+    void unpin(frame_id_t frame_id) override;
+    size_t Size() override;
+
+private:
+    struct ClockEntry
+    {
+        bool in_replacer{false}; // 是否在可替换队列中
+        bool ref{false};         // 引用位
+    };
+
+    struct Shard
+    {
+        std::mutex mtx;                  // 分片锁
+        std::vector<ClockEntry> entries; // 页面状态数组
+        size_t clock_hand{0};            // 时钟指针
+        size_t num_entries{0};           // 当前可替换的页面数
+    };
+
+    static constexpr int SHARD_COUNT = 16; // 分片数，减少锁竞争
+    std::vector<Shard> shards_;            // 分片数组
+
+    inline Shard &get_shard(frame_id_t frame_id);
+};
--- src/replacer/lru_replacer.h
+++ src/replacer/lru_replacer.h
@@ -1,47 +1,36 @@
-/* Copyright (c) 2023 Renmin University of China
-RMDB is licensed under Mulan PSL v2.
-You can use this software according to the terms and conditions of the Mulan PSL v2.
-You may obtain a copy of Mulan PSL v2 at:
-        http://license.coscl.org.cn/MulanPSL2
-THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-See the Mulan PSL v2 for more details. */
-
 #pragma once
 
 #include <list>
-#include <mutex>  
+#include <mutex>
 #include <vector>
-
+#include <unordered_map>
 #include "common/config.h"
 #include "replacer/replacer.h"
-#include "unordered_map"
 
-/*
-LRUReplacer实现了LRU替换策略
-*/
-class LRUReplacer : public Replacer {
-   public:
-    /**
-     * @description: 创建一个新的LRUReplacer
-     * @param {size_t} num_pages LRUReplacer最多需要存储的page数量
-     */
-    explicit LRUReplacer(size_t num_pages);
+class LRUReplacer : public Replacer
+{
+public:
+        explicit LRUReplacer(size_t num_pages);
+        ~LRUReplacer() = default;
 
-    ~LRUReplacer();
+        bool victim(frame_id_t *frame_id) override;
+        void pin(frame_id_t frame_id) override;
+        void unpin(frame_id_t frame_id) override;
+        size_t Size() override;
 
-    bool victim(frame_id_t *frame_id);
+private:
+        struct Shard
+        {
+                std::mutex mtx;
+                std::list<frame_id_t> lru_list;
+                std::unordered_map<frame_id_t, std::list<frame_id_t>::iterator> lru_map;
+        };
 
-    void pin(frame_id_t frame_id);
+        static constexpr int SHARD_COUNT = 16; // 根据CPU核心数调整
+        std::vector<Shard> shards_;
 
-    void unpin(frame_id_t frame_id);
-
-    size_t Size();
-
-   private:
-    std::mutex latch_;                  // 互斥锁
-    std::list<frame_id_t> LRUlist_;     // 按加入的时间顺序存放unpinned pages的frame id，首部表示最近被访问
-    std::unordered_map<frame_id_t, std::list<frame_id_t>::iterator> LRUhash_;   // frame_id_t -> unpinned pages的frame id
-    size_t max_size_;   // 最大容量（与缓冲池的容量相同）
+        inline Shard &get_shard(frame_id_t frame_id)
+        {
+                return shards_[frame_id & (SHARD_COUNT - 1)];
+        }
 };
